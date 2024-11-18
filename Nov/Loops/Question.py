0809169a116b9eee4a0a3d6819f534f15d2e3d9d
Ni3Gawande <nitu.gawande95@gmail.com>
2024-11-18 15:53:06 +0530

    Update Question.py

--- Nov/Loops/Question.py
+++ Nov/Loops/Question.py
@@ -1,10 +1,10 @@
 for i in range(1, 100):
     if i % 3 == 0 and i % 5 == 0:
-        print(f"{i}FizzBuzz")
+        print("FizzBuzz")
     elif i % 3 == 0:
-        print(f"{i}Fizz")
+        print("Fizz")
     elif i % 5 == 0:
-        print(f"{i}Buzz")
+        print("Buzz")
 
     else:
         pass
